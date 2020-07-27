99201c3ab1c09f09a35c137b463407a0c242c480
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-27 17:56:39 -0400

    seeds in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Restaurant.create([
-    {
-        
+User.destroy_all 
+Restaurant.destroy_all 
+Wellness_Center.destroy_all 
 
+5.times do 
+User.create([
+    { username: "NYC123",
+      password: "NYC123",
+      location: "brooklyn"
+    }
+])
+end
+
+50.times do
+Restaurant.create([
+    { name: Faker::Restaurant.name,
+    bio: Faker::Restaurant.type,
+    address: Faker::Address.street_address ,
+    url: Faker::Internet.url,
+    review: Faker::Restaurant.review ,
+    avg_rating: 5
 }])
+end
+
+50.times do
+Wellness_center.create([
+    { name: "Sun Studio" ,
+    bio: "Namaste",
+    address: Faker::Address.street_address ,
+    url: Faker::Internet.url,
+    review: "Amazing studio!",
+    avg_rating: 4
+}])
+end
+puts "done seeding"
