d7d1702738e0f2b777e00d441425aff9d742313c
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-28 12:16:15 -0400

    July 28 commit 1

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,15 +6,4 @@ class RestaurantsController < ApplicationController
     def show
         @restaurant = Restaurant.find(params[:id])
     end
-
-    def create 
-        @restaurant = Restaurant.create(restaurant_params)
-    end
-
-    private 
-    def restaurant_params
-        params.permit(:name, :bio, :address, :url, :review, :avg_rating)
-    end
-
-
 end
