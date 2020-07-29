d919a47780d958249b22154bcb5404df9c5405a3
Ginger-Mano <mcasimirian@gmail.com>
2020-07-29 18:14:10 -0400

    wth

--- app/models/restaurant_reservation.rb
+++ app/models/restaurant_reservation.rb
@@ -1,4 +1,12 @@
 class RestaurantReservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+
+  validates :party_size, numericality: {
+    greater_than: 0 
+  }
+  validates :date, numericality: {
+        greater_than: 0
+  }
 end
