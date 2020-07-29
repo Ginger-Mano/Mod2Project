5a01f2de94001279b95043d3d5bbfdb6274b236b
Ginger-Mano <mcasimirian@gmail.com>
2020-07-28 23:16:35 -0400

    last commit July 27

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :users
    get 'users/:id/showreservations', to: 'users#showreservations', as: 'showreservations'
    post '/restaurant_reservations/new', to: 'restaurant_reservations#create'
-   
+   get '/restaurant_reservations/:id/edit', to: 'restaurant_reservations#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
