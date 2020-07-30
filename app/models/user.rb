435bd22ee7bf1d16eb8f8f62d17e37c8b2022a21
Ginger-Mano <mcasimirian@gmail.com>
2020-07-30 12:50:18 -0400

    wth2 resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,8 @@ class User < ApplicationRecord
     has_many :wellness_reservations 
     has_many :wellness_centers, through: :wellness_reservations
 
-<<<<<<< HEAD
-
-    validates :username, presence: true
-    validates :username, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters!" }
-    validates :password, presence: true 
-    validates :password, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters!" }
-=======
 validates :username, presence: true
 validates :username, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters!" }
 validates :password, presence: true 
 validates :password, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters!" }
->>>>>>> s
 end
