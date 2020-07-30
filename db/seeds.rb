5b7d67a85f7acebfb869bf584fdc0e7aafd1e445
Ginger-Mano <mcasimirian@gmail.com>
2020-07-29 23:15:17 -0400

    thank god

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,60 +12,66 @@ Restaurant.destroy_all
 WellnessCenter.destroy_all 
 
 
+
+
+
+# 5.times do 
 5.times do 
-User.create([
-    { username: "NYC123",
-      password: "NYC123",
-      location: "brooklyn"
-}
-])
-end
-
-50.times do
-Restaurant.create([
-    { name: Faker::Restaurant.name,
-    bio: Faker::Restaurant.type,
-    address: Faker::Address.street_address ,
-    url: Faker::Internet.url,
-    review: Faker::Restaurant.review ,
-    avg_rating: 5
-}])
-end
-
-50.times do
-WellnessCenter.create([
-    { name: "Sun Studio" ,
-    bio: "Namaste",
-    address: Faker::Address.street_address ,
-    url: Faker::Internet.url,
-    review: "Amazing studio!",
-    avg_rating: 4
-}])
-end
-
-6.times do
-    RestaurantReservation.create([
-        {
-            restaurant_name: Restaurant.first.name,
-            party_size: 5,
-            date: "November 3",
-            time: "5:00",
-            user_id: User.first.id,
-            restaurant_id: Restaurant.first.id
+    User.create([
+        { username: "NYCred",
+          password: "NYClol",
+          location: "brooklyn"
         }
     ])
 end
 
-6.times do
-    WellnessReservation.create([
-        {
-            wellnesscenter_name: WellnessCenter.first.name,
-            group_size: 5,
-            date: "November 3",
-            time: "5:00",
-            user_id: User.first.id,
-            wellness_center_id: WellnessCenter.first.id
-        }
-    ])
-end
-puts "done seeding"
+50.times do
+    Restaurant.create([
+        { name: Faker::Restaurant.name,
+        bio: Faker::Restaurant.type,
+        address: Faker::Address.street_address ,
+        url: Faker::Internet.url,
+        review: Faker::Restaurant.review ,
+        avg_rating: 5
+    }])
+    end
+
+    50.times do
+        WellnessCenter.create([
+            { name: "Sun Studio" ,
+            bio: "Namaste",
+            address: Faker::Address.street_address ,
+            url: Faker::Internet.url,
+            review: "Amazing studio!",
+            avg_rating: 4
+        }])
+        end
+
+        6.times do
+            RestaurantReservation.create([
+                {
+                    restaurant_name: Restaurant.first.name,
+                    party_size: 5,
+                    date: "November 3",
+                    time: "5:00",
+                    user_id: User.first.id,
+                    restaurant_id: Restaurant.first.id
+                }
+            ])
+        end
+
+        6.times do
+            WellnessReservation.create([
+                {
+                    wellnesscenter_name: WellnessCenter.first.name,
+                    group_size: 5,
+                    date: "November 3",
+                    time: "5:00",
+                    user_id: User.first.id,
+                    # user_id: User.first.id,
+                    wellness_center_id: WellnessCenter.first.id
+                }
+            ])
+        end
+        puts "done seeding"
+
