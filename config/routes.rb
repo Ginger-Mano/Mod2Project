fe47e48e8bc07a9eb68d51e13bb76e8215ba5051
Ginger-Mano <mcasimirian@gmail.com>
2020-07-30 21:53:14 -0400

    images

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
 
    post '/wellness_reservations/new', to: 'wellness_reservations#create'
    get '/wellness_reservations/:id/edit', to: 'wellness_reservations#edit'
+
+   get '/about', to: 'static#about'
+   get '/assets/AdobeStock_260128861.jpeg', to: 'restaurants#index'
+   get '/assets/AdobeStock_163003185.jpeg', to: 'wellnesscenter#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
